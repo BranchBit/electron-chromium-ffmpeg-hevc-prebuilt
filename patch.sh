686c2ae62ed0b800156c6bb0324e2591f40c78db
WhiteWhidow <whitewhidow@users.noreply.github.com>
2025-08-05 15:07:20 +0200

    Add files via upload

---
+++ patch.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## follow https://www.electronjs.org/docs/latest/development/build-instructions-gn
+## change your source to checkout electron src v37.2.4
+## then run this after the gclient sync step
+
+
+electron_src_dir="/home/sam/electron/src" ## CHANGE THIS TO YOUR ELECTRON SOURCE DIRECTORY
+
+# Download the necessary files
+echo "Downloading necessary files..."
+curl https://raw.githubusercontent.com/StaZhu/enable-chromium-hevc-hardware-decoding/refs/tags/138.0.7191.0/add-hevc-ffmpeg-decoder-parser.js -o add-hevc-ffmpeg-decoder-parser.js
+curl https://raw.githubusercontent.com/StaZhu/enable-chromium-hevc-hardware-decoding/refs/tags/138.0.7191.0/add-hevc-ffmpeg-decoder-parser.patch -o add-hevc-ffmpeg-decoder-parser.patch
+curl https://raw.githubusercontent.com/StaZhu/enable-chromium-hevc-hardware-decoding/refs/tags/138.0.7191.0/change-libavcodec-header.patch -o change-libavcodec-header.patch
+curl https://raw.githubusercontent.com/StaZhu/enable-chromium-hevc-hardware-decoding/refs/tags/138.0.7191.0/enable-hevc-ffmpeg-decoding.patch -o enable-hevc-ffmpeg-decoding.patch
+
+
+# Check if the current directory is correct
+current_dir=$(pwd)
+echo "Current directory is: $current_dir"
+echo "Current directory is: $electron_src_dir"
+
+
+# Check if the Electron source directory exists
+if [ ! -d "$electron_src_dir" ]; then
+  echo "Directory $electron_src_dir does not exist. Please clone the Electron repository first."
+  exit 1
+fi
+
+# Change to the Electron source directory
+cd "$electron_src_dir" || { echo "Failed to change directory to $electron_src_dir"; exit 1; }
+
+
+# apply the patches from the old current dir to the new current dir
+echo "Changed directory to: $(pwd)"
+git apply --ignore-whitespace  "$current_dir/add-hevc-ffmpeg-decoder-parser.patch" --verbose
+git apply --ignore-whitespace  "$current_dir/enable-hevc-ffmpeg-decoding.patch" --verbose
+
+
+
+#change dir to old current dir/third_part/ffmpeg
+cd "$electron_src_dir/third_party/ffmpeg" || { echo "Failed to change directory to $electron_src_dir/third_party/ffmpeg"; exit 1; }
+echo "Changed directory to: $(pwd)"
+# apply the patches from the old current dir to the new current dir
+git apply --ignore-whitespace  "$current_dir/change-libavcodec-header.patch" --verbose
+node "$current_dir/add-hevc-ffmpeg-decoder-parser.js"
+
+
+#echo done finish script
+echo "Patches applied successfully."
+
+
+
+#add-hevc-ffmpeg-decoder-parser.js where ffmpeg_generated.gn is
+#Update ffmpeg patch
+#4 months ago
+#add-hevc-ffmpeg-decoder-parser.patch src
+#Update ffmpeg patch
+#2 weeks ago
+#change-libavcodec-header.patch before libavcodec
+#Update ffmpeg patch
+#2 weeks ago
+#enable-hevc-ffmpeg-decoding.patch src
